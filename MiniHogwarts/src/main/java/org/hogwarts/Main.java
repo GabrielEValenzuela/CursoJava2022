d8c1c70aa57022ee95f79960ecd49a786d3ecabb
francovega <francovega1234@gmail.com>
2022-05-10 18:17:06 -0300

    Prueba

--- MiniHogwarts/src/main/java/org/hogwarts/Main.java
+++ MiniHogwarts/src/main/java/org/hogwarts/Main.java
@@ -1,7 +1,10 @@
 package org.hogwarts;
 
+import org.hogwarts.logic.Wizard;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new Wizard("Harry" , "Potter", (byte) 28 );
     }
 }
