216d63339b74b4d15ab74d81d08a33641b00ccc2
CatiniMariano <catinimariano@gmail.com>
2022-05-14 12:11:11 -0300

    TRABAJO EN CLASE

---
+++ MiniHogwarts/src/main/java/org/hogwarts/logic/AnimalFantastico.java
@@ -0,0 +1,9 @@
+package org.hogwarts.logic;
+
+public interface AnimalFantasticos {
+    void comer();
+    void cagar();
+    void atacar();
+    void Caminar();
+    void Fly();
+}
---
+++ MiniHogwarts/src/main/java/org/hogwarts/logic/Fenix.java
@@ -0,0 +1,27 @@
+package org.hogwarts.logic;
+
+public class Fenix implements AnimalFantastico{
+    @Override
+
+
+
+    }
+    public void comer(){
+        System.out.println("Tengo hambre");
+                medio raro el fenix
+    }
+    public void cagar(){
+        System.out.println("Falta papel");
+    }
+    public void atacar(){
+        System.out.println("*Escupe fuego*");
+    }
+    public void Caminar(){
+        System.out.println("*Camina*");
+    }
+    public void Fly(){
+        System.out.println("*Vuela*");
+    }
+
+
+}
